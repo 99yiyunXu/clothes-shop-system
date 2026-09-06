2cb497377733a2259a96e703eb8bb338b40e818a
yiyunXu <yiyunxu@gmail.com>
2026-09-06 18:35:24 +0900

    update login function

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
@@ -9,5 +11,21 @@ public class HomeController {
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+    
+    
+    @GetMapping("/index2")
+    @ResponseBody
+    public String index2(
+            @RequestParam String userName,
+            @RequestParam String password) {
+
+        if ("admin".equals(userName)
+                && "password".equals(password)) {
+
+            return "OK";
+        } else {
+            return "NG";
+        }
     }
 }
