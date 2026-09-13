e6dc9f8e659160167e0661fc2d6de0b6564b6e4a
yiyunXu <yiyunxu@gmail.com>
2026-09-13 13:47:08 +0900

    update clothes shop system

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,31 +1,78 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
+    // user
+    private final List<User> users = List.of(
+            new User("admin", "123456"),
+            new User("luna", "131400"),
+            new User("lihua", "1234"),
+            new User("lisi", "5678"),
+            new User("test", "test123")
+    );
+
+   
     @GetMapping("/")
     public String index() {
         return "index";
     }
     
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
     
-    @GetMapping("/index2")
-    @ResponseBody
-    public String index2(
+    @PostMapping("/login")
+    public String login(
             @RequestParam String userName,
-            @RequestParam String password) {
+            @RequestParam String password,
+            Model model) {
 
-        if ("admin".equals(userName)
-                && "password".equals(password)) {
+        for (User user : users) {
 
-            return "OK";
-        } else {
-            return "NG";
+            if (user.getUserName().equals(userName)
+                    && user.getPassword().equals(password)) {
+
+                model.addAttribute("userName", userName);
+
+                return "home";
+            }
+        }
+
+        model.addAttribute(
+                "error",
+                "ユーザー名またはパスワードが正しくありません。"
+        );
+
+        return "index";
+    }
+
+    
+    static class User {
+
+        private String userName;
+        private String password;
+
+        public User(String userName, String password) {
+            this.userName = userName;
+            this.password = password;
+        }
+
+        public String getUserName() {
+            return userName;
+        }
+
+        public String getPassword() {
+            return password;
         }
     }
 }
